26b28fe32a0ed4a5fcc96b9f81cc55d4c7768b83
Julian Bilcke <julian.bilcke@gmail.com>
2018-10-14 13:35:09 +0200

    less ugly

--- index.js
+++ index.js
@@ -1,33 +1,27 @@
-'use strict';
+'use strict'
 
 class ProcessPool {
-  constructor(workers) {
-    this.id = 0;
-    this.pending = {};
-    // console.log("workers:", workers)
+  constructor (workers) {
+    this.id = 0
+    this.pending = {}
     this.workers = workers.map(instance => {
       return {
         instance: instance,
         load: 0
       }
     })
-    // console.log("this.workers:", this.workers)
     this.workers.forEach(worker => {
       worker.instance.on('message', (msg) => {
-        worker.load--;
-        // console.log("received data from childProcess:", msg);
+        worker.load--
         if (typeof msg !== 'undefined' && typeof msg.id === 'number') {
           const pending = this.pending[msg.id];
           if (typeof pending !== 'undefined') {
             if (typeof pending.error !== 'undefined') {
-              //console.log("rejecting", msg.error)
               pending.reject(msg.error)
             } else {
-              //console.log("resolving", msg.data)
               pending.resolve(msg.data)
             }
-            //console.log("cleaning after ourselves")
-            delete this.pending[msg.id];
+            delete this.pending[msg.id]
           } else {
             console.log("received an old message that has expired.. doing nothing")
           }
@@ -36,46 +30,34 @@ class ProcessPool {
     })
   }
 
-  call(api, func, data) {
-    /*
-    console.log("calling.. api: ", api);
-    console.log("calling.. func: ", func);
-    console.log("calling.. data: ", data);
-    */
+  call (api, func, data) {
     return new Promise((resolve, reject) => {
-      const id = this.id++;
+      const id = this.id++
+
       this.pending[id] = {
         resolve: resolve,
         reject: reject
       }
 
       if (this.workers.length == 0) {
-        console.log("no worker available");
         reject(new Error("no worker available"))
-        return;
+        return
       }
 
       // send a job to the worker with the least load (number of pending items)
-      const worker = this.workers.sort((a, b) => a.load - b.load)[0];
+      const worker = this.workers.sort((a, b) => a.load - b.load)[0]
 
-      worker.load++;
-      /*
-      console.log("send.. id: ", id);
-      console.log("send.. api: ", api);
-      console.log("send.. func: ", func);
-      console.log("send.. data: ", data);
-      */
+      worker.load++
       worker.instance.send({
         id: id,
         api: api,
         func: func,
         data: data
-      });
-
-    });
+      })
+    })
   }
 }
 
-module.exports = ProcessPool;
-module.exports.default = ProcessPool;
-module.exports.ProcessPool = ProcessPool;
+module.exports = ProcessPool
+module.exports.default = ProcessPool
+module.exports.ProcessPool = ProcessPool
